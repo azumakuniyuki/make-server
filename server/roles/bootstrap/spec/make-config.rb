193facda05fcae5ae1137775d5bdce52a8bd3fd3
azumakuniyuki <azumakuniyuki@gmail.com>
2015-08-21 00:17:16 +0900

    Improved test code: use MakeServer::Ansible

--- server/roles/bootstrap/spec/make-config.rb
+++ server/roles/bootstrap/spec/make-config.rb
@@ -5,9 +5,17 @@
 # |_.__/ \___/ \___/ \__|___/\__|_|  \__,_| .__/_/ |___/ .__/ \___|\___|
 #                                         |_|          |_|              
 # make-config
-workingdir = '/usr/local/src'
+ansiblevars = MakeServer::Ansible.load_variables
 
-describe file(workingdir) do
-  it { should be_directory }
+if defined?( ansiblevars['all'] ) then
+  v = ansiblevars['all']['buildroot']
+
+  describe file(v) do
+    it { should be_directory }
+    it { should be_owned_by 'root' }
+    it { should be_readable }
+    it { should be_writable }
+    it { should be_executable }
+  end
 end
 
