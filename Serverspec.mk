f267f28ed278578597917e0d2a0fb450622b4a9c
azumakuniyuki <azumakuniyuki@gmail.com>
2015-09-26 19:41:12 +0900

    Makefile for Serverspec tests

---
+++ Serverspec.mk
@@ -0,0 +1,28 @@
+# Makefile for make-server/Serverspec
+#  ____                                                           _    
+# / ___|  ___ _ ____   _____ _ __ ___ _ __   ___  ___   _ __ ___ | | __
+# \___ \ / _ \ '__\ \ / / _ \ '__/ __| '_ \ / _ \/ __| | '_ ` _ \| |/ /
+#  ___) |  __/ |   \ V /  __/ |  \__ \ |_) |  __/ (__ _| | | | | |   < 
+# |____/ \___|_|    \_/ \___|_|  |___/ .__/ \___|\___(_)_| |_| |_|_|\_\
+#                                    |_|                               
+# ---------------------------------------------------------------------------
+HEREIAM := $(shell pwd)
+PWDNAME := $(shell echo $(HEREIAM) | xargs basename)
+
+.DEFAULT_GOAL := test
+ROOTDIRECTORY := server
+MAKESERVERCTL := makeserverctl
+TOBEINSTALLED := serverspec highline rake
+INVENTORYFILE  = $(shell head -1 .default-inventoryfile)
+
+# ---------------------------------------------------------------------------
+.PHONY: clean all $(SUBDIRS)
+
+install:
+	gem install $(TOBEINSTALLED)
+
+test:
+	rake INVENTORY=$(INVENTORYFILE) spec
+
+clean:
+
